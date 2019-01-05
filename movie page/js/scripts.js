319d89786cc59dd1a0a70e19e135fb7a5d6ac02b
Tina Sedaghat <sedaghat.tina@gmail.com>
2019-01-04 20:07:37 -0500

    adding snippets

--- movie page/js/scripts.js
+++ movie page/js/scripts.js
@@ -31,6 +31,7 @@ $(document).ready(function(){
   })
 });
 
+// for making nav bar in mobile auto hide after clicking somewhere else
 $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
@@ -49,5 +50,61 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
   // Solution: force focus on the element that the click event fired on
   $("#navbarToggle").click(function (event) {
     $(event.target).focus();
+    console.log("done;")
   });
 });
+
+
+// for loading home-main-content.html, my-footer.html, and my-owlcarousel.html to index.html
+(function (global) {
+
+var dc = {};
+
+var homeHtml = "snippets/home-main-content.html";
+var footer = "snippets/my-footer.html";
+var my_owlcarousel = "snippets/my-owlcarousel.html";
+
+// Convenience function for inserting innerHTML for 'select'
+var insertHtml = function (selector, html) {
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
+
+// Show loading icon inside element identified by 'selector'.
+var showLoading = function (selector) {
+  var html = "<div class='text-center'>";
+  html += "<img src='images/ajax-loader.gif'></div>";
+  insertHtml(selector, html);
+};
+
+// On page load (before images or CSS)
+document.addEventListener("DOMContentLoaded", function (event) {
+
+// On first load, show home view
+showLoading("#main-content");
+$ajaxUtils.sendGetRequest(
+  homeHtml,
+  function (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  },
+  false);
+
+$ajaxUtils.sendGetRequest(
+  footer,
+  function (responseText) {
+    document.querySelector("#myfooter")
+      .innerHTML = responseText;
+  },
+  false);
+
+$ajaxUtils.sendGetRequest(
+  my_owlcarousel,
+  function (responseText) {
+    document.querySelector("#myowlcarousel")
+      .innerHTML = responseText;
+  },
+  false);
+});
+global.$dc = dc;
+})(window);
